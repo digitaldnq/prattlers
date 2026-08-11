ddd5bc7f12390ab85032cc6a7abf55f0e0659ec8
digitaldnq <denqcr19@gmail.com>
2026-08-11 17:45:17 +0500

    feat: init prattlers-backend microservice skeleton

---
+++ src/main/java/by/doni/prattlers/PrattlersApplication.java
@@ -0,0 +1,13 @@
+package by.doni.prattlers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrattlersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrattlersApplication.class, args);
+    }
+
+}
