b82ebaf1ab7f99812792a7a01bff6fcd9a7db9a1
digitaldnq <denqcr19@gmail.com>
2026-08-12 17:57:10 +0500

    feat: added UserRepository

---
+++ core/src/main/java/by/doni/core/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package by.doni.core.repository;
+
+import by.doni.core.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
