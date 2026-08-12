ebb69f51aa48c2a38ebdcc81398aa07d342766f3
digitaldnq <denqcr19@gmail.com>
2026-08-12 17:57:39 +0500

    feat: added UserController

---
+++ core/src/main/java/by/doni/core/web/controller/UserController.java
@@ -0,0 +1,34 @@
+package by.doni.core.web.controller;
+
+import by.doni.core.entity.User;
+import by.doni.core.service.UserService;
+import by.doni.core.web.dto.CreateUserRequest;
+import by.doni.core.web.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{userId}")
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long userId) {
+        var user = userService.findById(userId);
+        return ResponseEntity.ok(new UserDto(user.getId(), user.getUsername()));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody CreateUserRequest request) {
+        var user = new User(request.getUsername(), request.getPassword(), request.getRole());
+        var createdUser = userService.create(user);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new UserDto(createdUser.getId(), createdUser.getUsername()));
+    }
+}
