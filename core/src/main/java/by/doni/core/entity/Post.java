765396d167d3c6e7286138d93e463afc4903f6b5
digitaldnq <denqcr19@gmail.com>
2026-08-13 22:05:37 +0500

    feat: added entities Post and Subscription

---
+++ core/src/main/java/by/doni/core/entity/Post.java
@@ -0,0 +1,37 @@
+package by.doni.core.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Post implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(max = 120, message = "Максимальный размер текста - 120 символов")
+    private String text;
+
+    @Pattern(regexp = "^#.*", message = "Тэг должен начинаться с '#'")
+    private String tag;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User author;
+
+    public Post(String text, String tag) {
+        this.text = text;
+        this.tag = tag;
+    }
+}
