c2e95d181ce9bb24ee99f9ed39f410fc1d0b2e8a
digitaldnq <denqcr19@gmail.com>
2026-08-13 22:22:23 +0500

    feat: added post filter

---
+++ core/src/main/java/by/doni/core/repository/specification/PostFilter.java
@@ -0,0 +1,17 @@
+package by.doni.core.repository.specification;
+
+import lombok.Data;
+
+@Data
+public class PostFilter {
+
+    private String tag;
+
+    private Long authorId;
+
+    private String text;
+
+    private Integer pageSize = 10;
+
+    private Integer pageNumber = 0;
+}
