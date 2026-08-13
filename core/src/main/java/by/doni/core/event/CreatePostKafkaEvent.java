acd401eb25cb0a04331b5ae7bb747c49834c5d92
digitaldnq <denqcr19@gmail.com>
2026-08-13 22:23:49 +0500

    feat: added post kafka event

---
+++ core/src/main/java/by/doni/core/event/CreatePostKafkaEvent.java
@@ -0,0 +1,18 @@
+package by.doni.core.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreatePostKafkaEvent {
+
+    private Long postId;
+
+    private Long authorId;
+
+    private String username;
+
+}
