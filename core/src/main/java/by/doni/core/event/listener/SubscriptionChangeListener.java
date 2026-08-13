842d4054d9d0c0053a0275b5200554d82528c6e6
digitaldnq <denqcr19@gmail.com>
2026-08-13 22:25:42 +0500

    feat: added subscription change listener

---
+++ core/src/main/java/by/doni/core/event/listener/SubscriptionChangeListener.java
@@ -0,0 +1,18 @@
+package by.doni.core.event.listener;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SubscriptionChangeListener {
+
+    @EventListener
+    public void OnEvent(SubscriptionChangeListener event) {
+        log.info("Get event for subscription change: {}", event);
+    }
+
+}
