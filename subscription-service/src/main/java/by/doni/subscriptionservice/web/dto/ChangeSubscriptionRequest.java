038d609b0f0f29181674ff476775be8d40ee4982
digitaldnq <denqcr19@gmail.com>
2026-08-14 21:13:19 +0500

    feat: add change subscription request

---
+++ subscription-service/src/main/java/by/doni/subscriptionservice/web/dto/ChangeSubscriptionRequest.java
@@ -0,0 +1,17 @@
+package by.doni.subscriptionservice.web.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class ChangeSubscriptionRequest {
+
+    @NotNull
+    private Long followeeId;
+
+    @NotNull
+    private Long followerId;
+
+    @NotNull
+    private SubscriptionType subscriptionType;
+}
