a41fc3e0de9418903de927e5612d29710816ac69
digitaldnq <denqcr19@gmail.com>
2026-08-14 14:21:01 +0500

    feat: add page response

---
+++ core/src/main/java/by/doni/core/web/dto/PageResponse.java
@@ -0,0 +1,15 @@
+package by.doni.core.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResponse<T> {
+
+    private List<T> data;
+
+    private Integer totalPages = 0;
+}
