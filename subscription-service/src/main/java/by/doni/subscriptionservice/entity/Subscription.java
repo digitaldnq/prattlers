37d3eb3f713ec930ec7e17d8624cb7c48d819ce7
digitaldnq <denqcr19@gmail.com>
2026-08-14 17:30:33 +0500

    feat: add entity Subscription

---
+++ subscription-service/src/main/java/by/doni/subscriptionservice/entity/Subscription.java
@@ -0,0 +1,28 @@
+package by.doni.subscriptionservice.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Document(collection = "subscriptions")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Subscription {
+
+    @Id
+    private Long id;
+
+    private Set<Long> subscribersId = new HashSet<>();
+
+    public Subscription(Long id) {
+        this.id = id;
+    }
+}
