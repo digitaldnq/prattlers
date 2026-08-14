5aaa3b65cc8e0c0f67c2ca1cf60f2d238ca5c754
digitaldnq <denqcr19@gmail.com>
2026-08-14 18:01:21 +0500

    feat: add create post kafka listener

---
+++ subscription-service/src/main/java/by/doni/subscriptionservice/listener/CreatePostKafkaListener.java
@@ -0,0 +1,22 @@
+package by.doni.subscriptionservice.listener;
+
+import by.doni.subscriptionservice.model.CreatePostKafkaEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class CreatePostKafkaListener {
+
+    @KafkaListener(topics = "${app.kafka.topic}",
+            groupId = "${app.kafka.groupId}",
+            containerFactory = "createPostKafkaListenerContainerFactory")
+    public void listen(@Payload CreatePostKafkaEvent event) {
+        log.info("Received create post event {}", event);
+    }
+
+}
