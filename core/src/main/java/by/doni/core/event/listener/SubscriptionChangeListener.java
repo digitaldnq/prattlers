3ad2dcfe4043d915470754e00171a815f44d2efe
digitaldnq <denqcr19@gmail.com>
2026-08-14 14:23:26 +0500

    feat: add to SubscriptionChangeListener subscription client dependency and switch constructor with cases

--- core/src/main/java/by/doni/core/event/listener/SubscriptionChangeListener.java
+++ core/src/main/java/by/doni/core/event/listener/SubscriptionChangeListener.java
@@ -1,5 +1,8 @@
 package by.doni.core.event.listener;
 
+import by.doni.core.client.SubscriptionClient;
+import by.doni.core.event.SubscriptionChangeApplicationEvent;
+import by.doni.core.exception.PrattlersException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -10,9 +13,18 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class SubscriptionChangeListener {
 
+    private final SubscriptionClient client;
+
     @EventListener
     public void OnEvent(SubscriptionChangeApplicationEvent event) {
         log.info("Get event for subscription change: {}", event);
+
+        switch (event.getSubscriptionType()) {
+            case SUBSCRIBE -> client.subscribe(event.getFoloweeId(), event.getFollowerId());
+            case UNSUBSCRIBE -> client.unsubscribe(event.getFoloweeId(), event.getFollowerId());
+            case REMOVE -> client.deleteSubcription(event.getFoloweeId());
+            default -> throw new PrattlersException("Type not found: " + event.getSubscriptionType());
+        }
     }
 
 }
