007eb5cfe9a9683a5922f83a3380e4a1ac294e10
digitaldnq <denqcr19@gmail.com>
2026-08-14 17:28:51 +0500

    feat: add CreatePostKafkaEvent and kafka consumer configuration

---
+++ subscription-service/src/main/java/by/doni/subscriptionservice/model/CreatePostKafkaEvent.java
@@ -0,0 +1,13 @@
+package by.doni.subscriptionservice.model;
+
+import lombok.Data;
+
+@Data
+public class CreatePostKafkaEvent {
+
+    private Long postId;
+
+    private Long authorId;
+
+    private String username;
+}
