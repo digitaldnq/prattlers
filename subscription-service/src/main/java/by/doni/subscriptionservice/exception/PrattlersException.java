574cbd44b7f05cdf2572b95f9a7664b10bbc3f87
digitaldnq <denqcr19@gmail.com>
2026-08-14 17:30:13 +0500

    feat: add a basic exception for the entire subscription service

---
+++ subscription-service/src/main/java/by/doni/subscriptionservice/exception/PrattlersException.java
@@ -0,0 +1,22 @@
+package by.doni.subscriptionservice.exception;
+
+public class PrattlersException extends RuntimeException{
+    public PrattlersException() {
+    }
+
+    public PrattlersException(String message) {
+        super(message);
+    }
+
+    public PrattlersException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PrattlersException(Throwable cause) {
+        super(cause);
+    }
+
+    public PrattlersException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
