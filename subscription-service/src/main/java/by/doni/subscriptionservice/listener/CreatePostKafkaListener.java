a9d5a5954d1e3677ee5721fa372d00f8c2e24915
digitaldnq <denqcr19@gmail.com>
2026-08-18 01:59:49 +0500

    feat: notify subscribers via WebSocket when a post is created

--- subscription-service/src/main/java/by/doni/subscriptionservice/listener/CreatePostKafkaListener.java
+++ subscription-service/src/main/java/by/doni/subscriptionservice/listener/CreatePostKafkaListener.java
@@ -1,22 +1,32 @@
 package by.doni.subscriptionservice.listener;
 
 import by.doni.subscriptionservice.model.CreatePostKafkaEvent;
+import by.doni.subscriptionservice.model.CreatePostMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
+
+import java.text.MessageFormat;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class CreatePostKafkaListener {
 
+    private final SimpMessagingTemplate messagingTemplate;
+
     @KafkaListener(topics = "${app.kafka.topic}",
             groupId = "${app.kafka.groupId}",
             containerFactory = "createPostKafkaListenerContainerFactory")
     public void listen(@Payload CreatePostKafkaEvent event) {
         log.info("Received create post event {}", event);
+        var userMessage = MessageFormat.format("Автор {0} создал новый пост", event.getUsername());
+        var message = new CreatePostMessage(event.getPostId(), event.getAuthorId(), userMessage);
+
+        messagingTemplate.convertAndSend("/topic/messages", message);
     }
 
 }
